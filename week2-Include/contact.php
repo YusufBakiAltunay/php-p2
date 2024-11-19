bd18eff362a425eee540e7f808a3b2bb2ada21d6
YusufBakiAltunay <9025526@student.zadkine.nl>
2024-11-19 09:32:50 +0100

    Opdracht3-Forms

--- week2-Include/contact.php
+++ week2-Include/contact.php
@@ -10,6 +10,23 @@
     
     <h1>Welkom op de contact page</h1>
 
+    <form action="">
+
+       <label for="enaam">Naam:</label><br>
+       <input name="enaam" type="text"  id="fname"><br>
+
+       <label for="anaam">Achternaam:</label><br>
+       <input name="anaam" type="text" id="sname"><br>
+
+       <label for="bericht">Bericht:</label><br>
+       <input name="bericht" type="text" id="message"><br><br>
+
+       <button>Indienen</button>
+
+
+
+    </form>
+
 
     <?php  include 'footer.php';   ?>
 </body>
